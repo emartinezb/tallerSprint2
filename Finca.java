a42c2ecddf6f33b7466dbfc507ef2aad95ae29f0
emartinezb <estefa0223@gmail.com>
2022-08-29 18:43:45 -0500

    Creaación de clase Finca

---
+++ Finca.java
@@ -0,0 +1,47 @@
+public class Finca {
+/*
+    nombre
+    ciudad
+    area m2
+    telefono
+ */
+    private String nombre;
+    private String cuidad;
+    private float area;
+    private String telefono;
+
+
+
+    //Getters and Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuidad() {
+        return cuidad;
+    }
+
+    public void setCuidad(String cuidad) {
+        this.cuidad = cuidad;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
